c011e7ae62092297b52029ba7426354c321f04ed
Dominik Krupke <krupked@gmail.com>
2023-09-14 13:04:07 +0200

    base code

---
+++ tests/test_field_numbers.py
@@ -0,0 +1,7 @@
+from pyvispoly import FieldNumber, Point, Polygon, PolygonWithHoles
+
+
+def test_field_number():
+    x1 = FieldNumber(1)
+    x2 = FieldNumber(2)
+    assert x1 + x2 == FieldNumber(3)
